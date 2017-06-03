dd8b1ac27a5fa33aff62979bdeb202cfacfdcb0b
Kat Marchán <kzm@sykosomatic.org>
2017-06-03 15:27:40 -0700

    misc: added test and index.js placeholders

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// const BB = require('bluebird')
+
+// const cacache = require('cacache')
+// const fs = BB.promisifyAll(require('graceful-fs'))
+
+module.exports.get = getDir
+function getDir () {}
+
+module.exports.put = putDir
+function putDir () {}
---
+++ test/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const test = require('tap').test
+
+test('api is fully implemented')
