a57f634f4f61b0f8c6a50fe897450b32f221a807
Kat Marchán <kzm@sykosomatic.org>
2017-10-17 11:54:05 -0400

    fix(api): got everything working and passing tests

--- index.js
+++ index.js
@@ -5,34 +5,41 @@ const BB = require('bluebird')
 const cacache = require('cacache')
 const fs = BB.promisifyAll(require('graceful-fs'))
 const mkdirp = BB.promisify(require('mkdirp'))
+const packlist = require('npm-packlist')
 const path = require('path')
 
 const MAX_BULK_SIZE = 1 * 1024 * 1024 // 1MB
+
+const contentPath = require('cacache/lib/content/path.js')
 
 module.exports.get = get
 function get (cache, key, dest, opts) {
   opts = Object.assign({}, opts || {})
   delete opts.integrity
   return cacache.get(cache, key, opts).then(info => {
+    const dirs = new Set()
     const files = JSON.parse(info.data.toString('utf8'))
     return BB.map(Object.keys(files), f => {
       const fdest = path.join(dest, f)
       const fstat = files[f]
-      if (fstat.isDir) {
-        return mkdirp(fdest, {mode: opts.dmode || fstat.mode})
-        .then(() => fs.utimesAsync(
-          fdest, new Date(fstat.atime), new Date(fstat.mtime)
-        ))
-      } else {
-        return mkdirp(path.dirname(fdest))
-        .then(() => cacache.get.copy.byDigest(cache, fstat.integrity, fdest))
-        .then(() => fs.openAsync(fdest, 'w'))
-        .tap(fd => fs.fchmodAsync(fd, opts.fmode || fstat.mode))
-        .tap(fd => fs.futimesAsync(
-          fd, new Date(fstat.atime), new Date(fstat.mtime)
-        ))
-        .then(fd => fs.closeAsync(fd))
-      }
+      return BB.resolve()
+      .then(() => {
+        const dir = path.dirname(fdest)
+        if (!dirs.has(dir)) {
+          return mkdirp(dir).then(() => dirs.add(dir))
+        }
+      })
+      .then(() => {
+        if (opts.link) {
+          return fs.linkAsync(contentPath(cache, fstat.integrity), fdest)
+        } else {
+          return cacache.get.copy.byDigest(cache, fstat.integrity, fdest, {
+            mode: opts.fmode || fstat.mode,
+            mtime: fstat.mtime,
+            unsafe: opts.unsafe
+          })
+        }
+      })
     }, {concurrency: 100})
   })
 }
@@ -41,42 +48,43 @@ module.exports.put = put
 function put (cache, key, file, opts) {
   opts = Object.assign({}, opts || {})
   delete opts.integrity
-  return _put(cache, key, path.resolve(file), '', opts).then(meta => {
-    return cacache.put(cache, key, JSON.stringify(meta), opts)
-  })
+  return BB.resolve(packlist({path: file}))
+  .then(files => BB.map(files, f => {
+    return _put(cache, key, path.join(file, f), f, opts)
+  }), {concurrency: 10})
+  .then(stats => stats.reduce((acc, info) => {
+    if (info) {
+      acc[info.path] = info
+    }
+    return acc
+  }, {}))
+  .then(meta => cacache.put(cache, key, JSON.stringify(meta), opts))
 }
 
 function _put (cache, key, file, fname, opts) {
   opts = Object.assign({}, opts || {})
   delete opts.integrity
   return fs.statAsync(file).then(stat => {
-    if (stat.isDirectory()) {
-      return BB.map(fs.readdirAsync(file), f => {
-        return _put(cache, key, path.join(file, f), path.join(fname, f), opts)
-      }, {concurrency: 20}).then(files => {
-        return files.reduce((acc, info) => {
-          if (info) {
-            Object.assign(acc, info)
-          }
-          return acc
-        }, {[fname]: Object.assign({isDir: true}, stat)})
-      })
-    } else if (!stat.isFile()) {
+    if (!stat.isFile()) {
       // ignored. We don't do things like symlinks rn
     } else if (stat.size < MAX_BULK_SIZE) {
       return fs.readFileAsync(file).then(data => {
         return cacache.put(
           cache, `${key}:${fname}`, data, Object.assign({}, opts, {
+            mtime: +stat.time,
+            mode: stat.mode,
             metadata: stat
           })
         )
-      }).then(integrity => ({[fname]: Object.assign({integrity}, stat)}))
+      }).then(integrity => Object.assign({path: fname, integrity}, stat))
     } else {
       let integrity
       return BB.fromNode(cb => {
         const from = fs.createReadStream(file)
         const to = cacache.put.stream(
           cache, `${key}:${fname}`, Object.assign({}, opts, {
+            mtime: +stat.mtime,
+            mode: stat.mode,
             metadata: stat
           }
         )).on('integrity', i => { integrity = i })
@@ -84,7 +92,7 @@ function _put (cache, key, file, fname, opts) {
         to.on('error', cb)
         to.on('finish', () => cb())
         from.pipe(to)
-      }).then(() => ({[fname]: Object.assign({integrity}, stat)}))
+      }).then(() => Object.assign({path: fname, integrity}, stat))
     }
   }).catch({code: 'ENOENT'}, err => {
     if (err.code !== 'ENOENT') {
